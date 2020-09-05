2d915521eccc088d14bec021d89a53c6e9a04e45
merlin2181 <merlin2181@gmail.com>
2020-09-04 17:55:44 -0700

    Stage 3 of 6 of the Regex Engine project from JetBrains Academy
    - updated the regex function to use recursion to cycle through the search string until it finds the first character of the pattern and then attempts to find all chars from pattern in the search string.

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -1,4 +1,9 @@
 def regex_func(pattern, search_str):
+    """
+    Function that cycles through a regex pattern and search string to see if it can find the
+    pattern in the search string.  If it does, the function returns True. If it doesn't the
+    function returns False.
+    """
     if not pattern:
         return True
     if not search_str:
@@ -6,8 +11,19 @@ def regex_func(pattern, search_str):
     if pattern[0] == search_str[0] or pattern[0] == '.':
         return regex_func(pattern[1:], search_str[1:])
     else:
+        return regex_func(pattern, search_str[1:])
+
+
+def search_strings():
+    """
+    Function that asks the user for a search pattern and search string and passes the two to
+    the regex_function.  It returns a True or False value
+    """
+    regex, search = input().split('|')
+    if regex_func(regex, search):
+        return True
+    else:
         return False
 
 
-regex, search = input().split('|')
-print(regex_func(regex, search))
+print(search_strings())
